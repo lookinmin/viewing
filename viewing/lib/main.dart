c251314d526b87e042aa7dfc05ed10314ffc2f67
kkm0406 <sdjmc30412@naver.com>
2022-07-29 18:08:24 +0900

    홈화면 및 페이지 넘어가는거

--- viewing/lib/main.dart
+++ viewing/lib/main.dart
@@ -1,11 +1,14 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import './category/example.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Viewing());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Viewing extends StatelessWidget {
+  const Viewing({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -24,92 +27,177 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Home> createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeState extends State<Home> {
+  int currentIdx = 0;
+  final _pages = const [SplashRoute(), Setting(), SplashRoute(), Setting()];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  late List<GlobalKey<NavigatorState>> _navigatorKeyList;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _navigatorKeyList =
+        List.generate(_pages.length, (index) => GlobalKey<NavigatorState>());
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return WillPopScope(
+        onWillPop: () async {
+          return !(await _navigatorKeyList[currentIdx]
+              .currentState!
+              .maybePop());
+        },
+        child: Scaffold(
+          body: Column(
+            children: [
+              Flexible(flex: 1, fit: FlexFit.tight, child: AddressSearch()),
+              Expanded(
+                flex: 8,
+                child: IndexedStack(
+                  index: currentIdx,
+                  children: _pages.map((page) {
+                    int index = _pages.indexOf(page);
+                    return Navigator(
+                      key: _navigatorKeyList[index],
+                      onGenerateRoute: (_) {
+                        return MaterialPageRoute(builder: (context) => page);
+                      },
+                    );
+                  }).toList(),
+                ),
+              )
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: currentIdx,
+            type: BottomNavigationBarType.fixed,
+            onTap: (index) {
+              setState(() {
+                currentIdx = index;
+              });
+            },
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home_outlined,
+                ),
+                label: '홈',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.place_outlined,
+                ),
+                label: '지도',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.favorite_outline,
+                ),
+                label: '게시판',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person_outline,
+                ),
+                label: '내정보',
+              ),
+            ],
+            unselectedItemColor: Colors.black,
+            selectedItemColor: Color.fromRGBO(255, 99, 99, 1),
+            unselectedLabelStyle: TextStyle(color: Colors.black),
+            selectedLabelStyle:
+                TextStyle(color: Color.fromRGBO(255, 99, 99, 1)),
+          ),
+        ));
+  }
+}
+
+class MaingPage extends StatefulWidget {
+  const MaingPage({Key? key}) : super(key: key);
+
+  @override
+  State<MaingPage> createState() => _MaingPageState();
+}
+
+class _MaingPageState extends State<MaingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class AddressSearch extends StatelessWidget {
+  const AddressSearch({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      decoration: BoxDecoration(
+          border: Border(
+              bottom: BorderSide(
+        color: Colors.grey,
+        width: 2.0,
+      ))),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.4),
+                  spreadRadius: 0,
+                  blurRadius: 2,
+                  offset: Offset(0, 7))
+            ]),
+        child:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Flexible(
+            flex: 8,
+            child: TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: '검색할 주소를 입력하세요',
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
+          Expanded(
+              child: Icon(
+            Icons.search,
+            color: Color.fromRGBO(255, 99, 99, 1),
+          ))
+        ]),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class Setting extends StatelessWidget {
+  const Setting({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: TextButton(
+      child: const Text('Setting'),
+      // onPressed: () => Navigator.pushNamed(context, '/second'),
+      onPressed: () {},
+    ));
   }
 }
