cff448ecd6bc69a4725620cfd7df04ff36675eb4
JongWon <3338761@naver.com>
2022-07-30 21:53:33 +0900

    커스텀오버레이 설정 커스텀오버레이 추가 및 오버레이 클릭 이벤트 추가

--- viewing/lib/map.dart
+++ viewing/lib/map.dart
@@ -1,48 +1,50 @@
 import 'package:kakaomap_webview/kakaomap_webview.dart';
 import 'package:flutter/material.dart';
 
+const String kakaoMapKey = '404f1420684b6d045bcf36049efc3842';
+
 class Map extends StatelessWidget {
   const Map({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: KakaoMapTest(),
-    );
-  }
-}
-
-const String kakaoMapKey = '404f1420684b6d045bcf36049efc3842';
-
-class KakaoMapTest extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            // KakaoMapView
-            KakaoMapView(
-                width: size.width,
-                height: 200,
-                kakaoMapKey: kakaoMapKey,
-                lat: 33.450701,
-                lng: 126.570667,
-                showMapTypeControl: true,
-                showZoomControl: true,
-                markerImageURL:
-                    'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
-                onTapMarker: (message) async {
-                  ScaffoldMessenger.of(context)
-                      .showSnackBar(SnackBar(content: Text('Marker is clicked')));
+      child: KakaoMapView(
+          width: size.width,
+          height: 200,
+          kakaoMapKey: kakaoMapKey,
+          lat: 33.450701,
+          lng: 126.570667,
+          showZoomControl: true,
+          customOverlayStyle: '''<style>
+                  .customoverlay {position:relative;bottom:85px;border-radius:6px;border: 1px solid #ccc;border-bottom:2px solid #ddd;float:left;}
+    .customoverlay:nth-of-type(n) {border:0; box-shadow:0px 1px 2px #888;}
+    .customoverlay a {display:block;text-decoration:none;color:#000;text-align:center;border-radius:6px;font-size:14px;font-weight:bold;overflow:hidden;background: #d95050;background: #d95050 url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/arrow_white.png) no-repeat right 14px center;}
+    .customoverlay .title {display:block;text-align:center;background:#fff;margin-right:35px;padding:10px 15px;font-size:14px;font-weight:bold;}
+    .customoverlay:after {content:'';position:absolute;margin-left:-12px;left:50%;bottom:-12px;width:22px;height:12px;background:url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png')}
+                  </style>''',
+          customOverlay: '''
+    var content = '<div class="customoverlay" onclick="temp()">' +
+        '    <span class="title">카카오!</span>' +
+        '</div>';
 
-                  //await _openKakaoMapScreen(context);
-                }),
-          ],
-        ),
+    var position = new kakao.maps.LatLng(33.450701, 126.570667);
+
+    var customOverlay = new kakao.maps.CustomOverlay({
+        map: map,
+        position: position,
+        content: content,
+        yAnchor: 1
+    });
+    function temp() {
+        onTapMarker.postMessage('marker hjhjhjis tapped');
+    }
+                  ''',
+          onTapMarker: (message) {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text(message.message)));
+          }),
     );
   }
-
-
 }
