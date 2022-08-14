64b28c50cd78bf8763af9193f66c56d7624f0c69
MINSU <ancx1234@naver.com>
2022-08-14 21:06:01 +0900

    민수/web/viewing 웹사이트 구현

---
+++ viewing-website/src/components/Footer.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+export const Footer = () => {
+
+
+  const move=(e)=> {
+    switch (e){
+      case 1:
+        window.open('https://www.instagram.com/lookin_min/');
+        break;
+      case 2:
+        window.open('https://www.apple.com/kr/app-store/');
+        break;
+      case 3:
+        window.open('https://play.google.com/store/games?hl=ko');
+        break;
+      case 4:
+        window.open('https://github.com/lookinmin/viewing');
+        break;
+    }
+  }
+
+  
+  return (
+    <div className="footer">
+      <div className="pageIcons">
+        <img className='icons' src="./img/instagram.png"  onClick={() => move(1)} alt="fing insta"  height="40px"/>
+        <img className='icons' src="./img/app-store.png"  onClick={() => move(2)} alt="fing insta" height="40px"/>
+        <img className='icons' src="./img/playstore.png"  onClick={() => move(3)} alt="fing insta" height="40px"/>
+        <img className='icons' src="./img/github.png" onClick={() => move(4)} alt="fing insta" height="40px"/>
+      </div>
+      <div className="footerLogo">
+        <h2>Viewing : Review + viewing. 리뷰를 보다.</h2>
+      </div>
+
+      <div className="txts">
+        <div className="div1">
+          <p><span className='TT'>Address.</span>충청북도 청주시 서원구 충대로 1 충북대학교 S1-4 116호</p>
+          <p><span className='TT'>Team.</span>제 10회 K-해커톤 충청권 예선통과팀. 핑퐁</p>
+          
+        </div>
+        
+        <div className="div2">
+          <p><span className='TT'>E-mail.</span>sncalphs@gmail.com</p>
+          <p><span className='TT'>Instagram.</span>@lookin_min</p>
+        </div>
+        
+        <div className="div3">
+          <p><span className='TT'>Leader.</span>조민수</p>
+          <p>@ Copyright 2022 Viewing All rights reserved</p>
+        </div>
+      </div>
+
+      
+    </div>
+  )
+}
---
+++ viewing-website/src/components/Home.jsx
@@ -0,0 +1,128 @@
+import { Footer } from './Footer'
+import { Menu } from './Menu'
+import { MdRoom, MdHomeFilled, MdOutlineAttachMoney } from 'react-icons/md';
+import styles from './Home.module.css';
+import React, { useRef, useState, useEffect } from 'react';
+import { useIntersectionObserver } from './useIntersectionObserver.ts';
+import { IoAnalyticsOutline,IoPartlySunnyOutline, IoClipboardOutline } from "react-icons/io5";
+import { GiMoneyStack, GiPaperClip } from "react-icons/gi";
+import { GrClipboard } from "react-icons/gr";
+
+
+
+export const Home = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);      //현재 스크롤 하는 부분이 어딘지 받아올 State
+  const updateScroll = () => {
+    setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+  }
+  useEffect(()=>{
+      window.addEventListener('scroll', updateScroll);
+  });
+
+  const moveTop = () => {                                   //로고 누르면 최상단으로 이동하는 코드
+    window.scrollTo({top: 0, behavior : "smooth"});
+  }
+   
+
+  const target1 = useRef(null)
+  const target2 = useRef(null)
+  const target3 = useRef(null)
+  const target4 = useRef(null)
+
+  const img1 = useRef(null)
+  const img2 = useRef(null)
+  const img3 = useRef(null)
+                                                                  //스크롤 구간에 맞춰 Fing 글자 Fade IN
+  useIntersectionObserver(target1, styles.animation);
+  useIntersectionObserver(target2, styles.animation);
+  useIntersectionObserver(target3, styles.animation);
+  useIntersectionObserver(target4, styles.animation);
+
+  useIntersectionObserver(img1, styles.imgAni);
+  useIntersectionObserver(img2, styles.imgAni);
+  useIntersectionObserver(img3, styles.imgAni);
+
+  return (
+    
+    <div className="home">
+      <div className='mainPage'>
+        <h2 id='logo' onClick={() => moveTop()} className={scrollPosition < 1000 ? "white" : "orange"}>Viewing</h2>
+        <div className="top">
+          <h2>당신의 새로운 집</h2>
+          <h2>Viewing과 함께<MdHomeFilled size="70"/></h2>
+          
+        </div>
+      </div>
+
+      <Menu/>
+
+      <div className="toDownload">
+        <img src="./img/fingLogo.png" alt="fing" height="70px"/>
+        <h2>viewing 설치</h2>
+        <img id='goPS' src="./img/playstore.png" alt="fing" height="30px"/>
+      </div>
+
+      <div className="txtsBox">
+        <h2 id='y1'>설레이는 자취생활</h2>
+        <h2 id='y2'><span className='t1' ref={target1}>Viewing<MdHomeFilled size="60" color= '#FF6363'/></span>으로부터</h2>
+      </div>
+
+      <div className="itemsList">
+        <div className="items">
+          <div className="box">
+            <span className='icons'><GiPaperClip size="50"/></span>
+          </div>
+          <h2>실 거주자들의 <span className='t2'>찐</span> 거주후기</h2>
+        </div>
+        <div className="items">
+          <div className="box">
+            <span className='icons'><GiMoneyStack size="50"/></span>
+          </div>
+          <h2><span className='t2'>지역</span>별로 볼 수 있는 평균금액</h2>
+        </div>
+        <div className="items">
+          <div className="box">
+            <span className='icons'><IoClipboardOutline size="50"/></span>
+          </div>
+          <h2><span className='t2'>소통</span>을 위한 자유게시판</h2>
+        </div>
+      </div>
+
+      <div className="exOne">
+        <div className="txtBox">
+          <h2>페스티벌이 열리는 모든곳에</h2>
+          <h3>페스티벌에 대한 정보는</h3>
+          <h3><span className='t1' ref={target2}>Fing</span>과 함께</h3>
+        </div>
+        <img className='homeImg' ref={img1} src="./img/pic1.jpg" height="500px" alt="" />
+      </div>
+
+      
+      <div className="exOne">
+        <div className="txtBox">
+          <h2>지역 소상공인들을 위한</h2>
+          <h3>상생하는 페스티벌은</h3>
+          <h3><span className='t1' ref={target3}>Fing</span>과 함께</h3>
+        </div>
+        <img className='homeImg' ref={img2} src="./img/pic2.jpg" height="500px" alt="" />
+      </div>
+
+      <div className="exOne">
+        <div className="txtBox">
+          <h2>페스티벌만 보고 갈거야?</h2>
+          <h3>근처 놀거리 먹거리 역시</h3>
+          <h3><span className='t1' ref={target4}>Fing</span>과 함께</h3>
+        </div>
+        <img className='homeImg' ref={img3} src="./img/pic3.jpg" height="500px" alt="" />
+      </div>
+
+
+     <Footer></Footer>
+
+    </div>
+
+    
+    
+
+  )
+}
