81f29473b455d03f3a89b3e8e1792a0042d10814
kkm0406 <sdjmc30412@naver.com>
2022-08-21 23:06:54 +0900

    광모/방 리뷰 작성 페이지 최종

--- viewing/lib/writing/roomreviewsecond.dart
+++ viewing/lib/writing/roomreviewsecond.dart
@@ -35,26 +35,18 @@ class _RoomReviewSecondState extends State<RoomReviewSecond> {
 
   File? _image;
   final picker = ImagePicker();
+  List imageList = [];
+  int imageCnt = 0;
 
   // 비동기 처리를 통해 카메라와 갤러리에서 이미지를 가져온다.
   Future getImage(ImageSource imageSource) async {
     final image = await picker.pickImage(source: imageSource);
 
     setState(() {
-      _image = File(image!.path); // 가져온 이미지를 _image에 저장
+      // _image = File(image!.path); // 가져온 이미지를 _image에 저장
+      imageList.add(File(image!.path));
+      // imageCnt++;
     });
-  }
-
-  // 이미지를 보여주는 위젯
-  Widget showImage() {
-    return Container(
-        color: const Color(0xffd0cece),
-        width: MediaQuery.of(context).size.width * 0.3,
-        height: MediaQuery.of(context).size.width * 0.3,
-        child: Center(
-            child: _image == null
-                ? Text('No image selected.')
-                : Image.file(File(_image!.path))));
   }
 
   @override
@@ -73,11 +65,10 @@ class _RoomReviewSecondState extends State<RoomReviewSecond> {
         padding: EdgeInsets.symmetric(horizontal: 15),
         child: Column(
           children: [
-            ratingBar(),
+            buildingScore(),
             goodOrBad(context, '장점', goodText),
             goodOrBad(context, '단점', badText),
             confirmation(context),
-            // showImage(),
             uploadPic(),
           ],
         ),
@@ -89,10 +80,192 @@ class _RoomReviewSecondState extends State<RoomReviewSecond> {
               primary: Color.fromRGBO(255, 99, 99, 1),
             ),
             onPressed: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Home()));
+              showModalBottomSheet<void>(
+                context: context,
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                    side: BorderSide.none,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20))),
+                builder: (BuildContext context) {
+                  return StatefulBuilder(
+                      builder: (BuildContext context, setState) => SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.4,
+                            child: modalColumn(context),
+                          ));
+                },
+              ).then((value) {
+                setState(() {});
+              });
             },
-            child: Text('작성완료')),
+            child: Text('확인')),
+      ),
+    );
+  }
+
+  Column modalColumn(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        //상단 x 버튼
+        Flexible(
+          flex: 1,
+          child: Row(
+            children: [
+              Expanded(child: Container()),
+              IconButton(
+                icon: Icon(
+                  Icons.close_rounded,
+                ),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          ),
+        ),
+        Flexible(
+          flex: 2,
+          child: Text(
+            '리뷰를 작성해줘서 고마워요!\n당신이 생각하는 이 곳의 만족도는?',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 17, fontWeight: FontWeight.w800),
+          ),
+        ),
+        Flexible(
+          flex: 4,
+          child: ratingBar(),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromRGBO(255, 99, 99, 1),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (context) => Home()));
+                },
+                child: Text('작성완료')),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Container buildingScore() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(bottom: 10),
+              child: Text(
+                '건물 정보',
+                style: TextStyle(fontSize: 17),
+              )),
+          Container(
+            decoration: BoxDecoration(
+                color: Color.fromRGBO(99, 99, 99, 0.05),
+                borderRadius: BorderRadius.circular(8)),
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('방음'),
+                    RatingBar.builder(
+                      initialRating: 3,
+                      minRating: 1,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemCount: 5,
+                      itemSize: 30,
+                      itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                      itemBuilder: (context, _) => Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      onRatingUpdate: (rating) {},
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('청결'),
+                    RatingBar.builder(
+                      initialRating: 3,
+                      minRating: 1,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemCount: 5,
+                      itemSize: 30,
+                      itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                      itemBuilder: (context, _) => Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      onRatingUpdate: (rating) {},
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('시설'),
+                    RatingBar.builder(
+                      initialRating: 3,
+                      minRating: 1,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemCount: 5,
+                      itemSize: 30,
+                      itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                      itemBuilder: (context, _) => Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      onRatingUpdate: (rating) {},
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('안전'),
+                    RatingBar.builder(
+                      initialRating: 3,
+                      minRating: 1,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemCount: 5,
+                      itemSize: 30,
+                      itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                      itemBuilder: (context, _) => Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      onRatingUpdate: (rating) {},
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          //
+        ],
       ),
     );
   }
@@ -180,42 +353,93 @@ class _RoomReviewSecondState extends State<RoomReviewSecond> {
     );
   }
 
-  SizedBox uploadPic() {
-    return SizedBox(
-      height: 200,
-      child: Column(
-        children: [
-          Container(
-            alignment: Alignment.centerLeft,
-            margin: EdgeInsets.only(bottom: 20),
-            child: Text(
-              '사진을 올려주세요(선택)',
-              style: TextStyle(fontSize: 17),
-            ),
+  Column uploadPic() {
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.centerLeft,
+          margin: EdgeInsets.only(bottom: 20),
+          child: Row(
+            children: [
+              Text(
+                '사진을 올려주세요(선택)',
+                style: TextStyle(fontSize: 17),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                '사진은 5개까지 선택가능합니다',
+                style: TextStyle(color: Colors.blue, fontSize: 10),
+              )
+            ],
           ),
-          DottedBorder(
-            borderType: BorderType.RRect,
-            radius: Radius.circular(8),
-            strokeWidth: 3,
-            color: Colors.grey,
-            child: Container(
-              height: 150,
-              width: 150,
-              child: Center(
-                  child: IconButton(
-                      iconSize: 40,
-                      onPressed: () {
-                        getImage(ImageSource.gallery);
-                      },
-                      icon: Icon(
-                        Icons.add_a_photo,
-                        color: Colors.blue,
-                      ))),
-            ),
-          ),
-        ],
-      ),
+        ),
+        imageList.isEmpty
+            ? DottedBorder(
+                borderType: BorderType.RRect,
+                radius: Radius.circular(8),
+                strokeWidth: 3,
+                color: Colors.grey,
+                child: SizedBox(
+                  height: 150,
+                  width: 150,
+                  child: Center(
+                      child: IconButton(
+                          iconSize: 40,
+                          onPressed: () {
+                            getImage(ImageSource.gallery);
+                          },
+                          icon: Icon(
+                            Icons.add_a_photo,
+                            color: Colors.blue,
+                          ))),
+                ),
+              )
+            : Column(
+                children: [
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        for (int i = 0; i < imageList.length; i++) ...[
+                          showImage(imageList[i])
+                        ]
+                      ],
+                    ),
+                  ),
+                  DottedBorder(
+                    borderType: BorderType.RRect,
+                    radius: Radius.circular(8),
+                    strokeWidth: 3,
+                    color: Colors.grey,
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.6,
+                      child: IconButton(
+                          iconSize: 40,
+                          onPressed: () {
+                            getImage(ImageSource.gallery);
+                          },
+                          icon: Icon(
+                            Icons.add_a_photo,
+                            color: Colors.blue,
+                          )),
+                    ),
+                  )
+                ],
+              )
+      ],
     );
+  }
+
+  // 이미지를 보여주는 위젯
+  Widget showImage(imageList) {
+    return Container(
+        margin: EdgeInsets.fromLTRB(5, 0, 5, 10),
+        color: Color.fromARGB(255, 239, 238, 238),
+        width: MediaQuery.of(context).size.width * 0.2,
+        height: MediaQuery.of(context).size.width * 0.2,
+        child: Image.file(imageList));
   }
 
   Container goodOrBad(
@@ -263,13 +487,6 @@ class _RoomReviewSecondState extends State<RoomReviewSecond> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Container(
-              alignment: Alignment.centerLeft,
-              margin: EdgeInsets.only(bottom: 10),
-              child: Text(
-                '청주시 서원구 충대로 1에 대한 별점을 매겨주세요',
-                style: TextStyle(fontSize: 17),
-              )),
           Container(
             decoration: BoxDecoration(
                 color: Color.fromRGBO(99, 99, 99, 0.05),
